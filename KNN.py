05ee8254a4791a2cae06cb83618f43b370e56942
DannyG-creator <78911070+DannyG-creator@users.noreply.github.com>
2021-02-22 11:19:33 +0000

    Initial commit

---
+++ KNN.py
@@ -0,0 +1,22 @@
+import sklearn
+from sklearn.utils import  shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+data = pd.read_csv("car.data")
+print(data.head())
+
+le = preprocessing.LabelEncoder()
+buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+door = le.fit_transform(list(data["door"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+cls = le.fit_transform(list(data["cls"]))
+
+print(buying)
+
+predict = "class"
